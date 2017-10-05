30e23c1abcd0ce5ad9d6aed7bcc962e84f64e0f9
Espen Hovlandsdal <espen@hovlandsdal.com>
2017-10-05 10:55:16 +0200

    Fix warnings on missing keys, empty arrays

---
+++ test/fixtures/019-keyless.js
@@ -0,0 +1,39 @@
+module.exports = {
+  input: [
+    {
+      _type: 'block',
+      children: [
+        {
+          _type: 'span',
+          marks: [],
+          text: 'sanity'
+        },
+        {
+          _type: 'span',
+          marks: [],
+          text: ' is a full time job'
+        }
+      ],
+      markDefs: [],
+      style: 'normal'
+    },
+    {
+      _type: 'block',
+      children: [
+        {
+          _type: 'span',
+          marks: [],
+          text: 'in a world that '
+        },
+        {
+          _type: 'span',
+          marks: [],
+          text: 'is always changing'
+        }
+      ],
+      markDefs: [],
+      style: 'normal'
+    },
+  ],
+  output: '<div><p>sanity is a full time job</p><p>in a world that is always changing</p></div>'
+}
