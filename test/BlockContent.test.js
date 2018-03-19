8eaf7c7be0a8d688c4d9b7bf386b578b1e5ca4bf
Espen Hovlandsdal <espen@hovlandsdal.com>
2018-03-19 14:46:27 +0100

    Fix incorrect test - check returned HTML instead of props

--- test/BlockContent.test.js
+++ test/BlockContent.test.js
@@ -16,5 +16,5 @@ const normalize = html =>
 runTests({render, h, normalize, getImageUrl})
 
 test('renders empty block with react proptype error', () => {
-  expect(h(BlockContent, {})).toMatchSnapshot()
+  expect(render({})).toMatchSnapshot()
 })
