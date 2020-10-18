c055f68ff0896fd25c3757c9b1dcf61b5c680f96
Balázs Orbán <info@balazsorban.com>
2020-10-18 11:28:44 +0200

    update example

--- src/example.tsx
+++ src/example.tsx
@@ -1,41 +1,65 @@
-import {Block} from 'types'
+import {PortableText, TypeSerializer, MarkSerializers} from 'types'
 import BlockContent from './BlockContent'
 type CustomTypes = {
   note: {
     title: string
-    content: Block
+    content: PortableText<CustomTypes>
   }
   button: {color: 'red' | 'blue' | 'white'}
 }
+type CustomMarks = {
+  highlight: {
+    color: 'yellow' | 'orange'
+  }
+}
 const PortableText = () => (
-  <BlockContent<CustomTypes>
+  <BlockContent<CustomTypes, CustomMarks>
     serializers={{
       types: {
+        button: Button,
         note(props) {
-          console.log(props.node.title, props.node.content)
-          return null
-        },
-        button(props) {
-          console.log(props.node.color)
+          console.log(props)
           return null
         },
       },
+      marks,
     }}
     blocks={[
       {
-        _type: 'note',
-        content: {
-          _type: 'block',
-          children: [
-            {
-              _type: 'span',
-              text: 'Some text',
-            },
-          ],
-        },
-        title: 'Note title',
+        _type: 'button',
+        color: 'blue',
+      },
+      {
+        _type: 'block',
+        markDefs: [
+          {
+            _key: 'highlight',
+            color: 'orange',
+          },
+        ],
+        children: [
+          {
+            _type: 'span',
+            text: '',
+            marks: ['highlight'],
+          },
+        ],
       },
     ]}
   />
 )
 export default PortableText
+
+const Button: TypeSerializer<CustomTypes, 'button'> = (props) => {
+  console.log(props)
+
+  return null
+}
+
+const marks: MarkSerializers<CustomMarks> = {
+  highlight(props) {
+    console.log(props)
+
+    return null
+  },
+}
